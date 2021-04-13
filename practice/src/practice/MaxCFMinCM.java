58b3cb98918d6816217936bc2d7fc1d22f6386ca
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-04-13 16:48:08 +0900

    0413

---
+++ practice/src/practice/MaxCFMinCM.java
@@ -0,0 +1,21 @@
+package practice;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12940
+public class MaxCFMinCM { //최대공약수와 최소공배수
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        while(true) {
+        	int cnt=1;
+        	
+        }
+        
+        return answer;
+    }
+
+}
