d2e3fc165fdd875d524dd173d99762629c858436
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-04-14 16:43:47 +0900

    0414(2)

--- practice/src/practice/MaxCFMinCM.java
+++ practice/src/practice/MaxCFMinCM.java
@@ -8,14 +8,25 @@ public class MaxCFMinCM { //최대공약수와 최소공배수
 	}
 	
 	public static int[] solution(int n, int m) {
-        int[] answer = new int[2];
-        
+        int cnt = 2;
+        int cf=1; //최대공약수
+        int cm=1; //최소공배수
+        int min = n;
+        int mok1=n;
+        int mok2=m;
+        if(n<m) min=m;
         while(true) {
-        	int cnt=1;
-        	
-        }
+        	if(mok1/cnt ==0 || mok2/cnt ==0) {
+        		cf = cf * cnt;
+        		mok1 = mok1/cnt;
+        		mok2 = mok2/cnt;
+        		cnt=2;
+        	} else {
+        		cnt++;
+        	}
+        	if(cnt == min) break;
+        } //마지막 몫이 안나누어 떨어질때
         
-        return answer;
     }
 
 }
