e782a15484129726d657cf6897a86f64a78ea20e
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-04-22 16:18:24 +0900

    0422

---
+++ practice/src/practice/HandlingStrings.java
@@ -0,0 +1,17 @@
+package practice;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12918
+public class HandlingStrings {
+
+	public static void main(String[] args) {
+		String s ="a234";
+		System.out.println(solution(s));
+	}
+	
+    public static boolean solution(String s) {
+        boolean answer = true;
+        
+        
+        return answer;
+    }
+}
