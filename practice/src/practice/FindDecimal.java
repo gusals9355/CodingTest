c3ce6cbf2a53d60be5a6cc58fd629f6a35f25e5a
조현민 <72421969+gusals9355@users.noreply.github.com>
2021-05-05 23:03:28 +0900

    0505

--- practice/src/practice/FindDecimal.java
+++ practice/src/practice/FindDecimal.java
@@ -5,23 +5,28 @@ public class FindDecimal {
 
 	public static void main(String[] args) {
 		int n=10;
-		System.out.println(solution(n));
+		solution(n);
 	}
 	
 	public static int solution(int n) {
-		int answer=1; //이미 소수로 포함된 후 시작
+		int answer=0;
 		
-		for(int i=3; i<=n; i++) {
-			for(int j=2; j<i; j++) {
-				if(i>2 && i%2 == 0) break;
-				else if(i%j==0) break;
-				else if(j==i-1) {
-					answer++;
-				}
-			}
-		}
+        for(int i=2; i<=n; i++){
+            boolean verify = true;
+            for(int j=2; j<=Math.sqrt(i); j++){
+                if(i%j == 0) {
+                	verify = false;
+                	break;
+                }
+            }
+            if(verify) {
+            	System.out.println(i);
+            	answer++;
+            	
+            }
+        }
+		
 		return answer;
 	}
 
 }
-//2 3 5 7 배수는 모두 지움
---
+++ practice/src/practice/test.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println(Math.sqrt(2));
+	}
+
+}
