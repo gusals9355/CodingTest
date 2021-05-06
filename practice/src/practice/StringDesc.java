4d5e7ce1d5ac6d21ba99e5784091638dec583c3f
조현민 <72421969+gusals9355@users.noreply.github.com>
2021-05-06 22:00:22 +0900

    Add files via upload

--- practice/src/practice/StringDesc.java
+++ practice/src/practice/StringDesc.java
@@ -1,40 +1,28 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 //https://programmers.co.kr/learn/courses/30/lessons/12917
 public class StringDesc {
 
 	public static void main(String[] args) {
-		String s = "AabBA";
+		String s = "aBA";
 		solution(s);
 	}
 	
     public static String solution(String s) {
-    	char[] answer= new char[s.length()];
-    	
-    	for(int i=0; i<answer.length; i++) {
-    		answer[i] = s.charAt(i);
-    	}
-    	
-    	for(int i=0; i<answer.length-1; i++) {
-    		int temp = i;
-    		char temp2;
-    		
-    		for(int j=1; j<answer.length; j++) {
-    			if(answer[temp] >= answer[j]){
-    				temp2 = answer[j];
-    			}
-    			temp2 = answer[i];
-    			answer[i] = answer[temp];
-    			answer[temp] = answer[temp2];
-    		}
-    	}
-    	
-    	String a="";
-    	for (char c : answer) {
-			a+=c;
-		}
-    	
-        return a;
+	String answer = "";
+	String[] arr = s.split("");
+	List<String> list = new ArrayList<>();
+	
+	for (String string : arr) list.add(string);
+	Collections.sort(list, Collections.reverseOrder());
+	
+	for(String i : list) answer+=i;
+	System.out.println(answer);
+	return answer;
     }
 
 }
